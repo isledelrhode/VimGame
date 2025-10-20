14a8329399c7919713192c19c59caa7279dfbeea
Reed Stevens <Reed.El.Stevens@gmail.com>
2025-10-20 11:39:31 -0600

    Add basic JFrame structure

---
+++ src/Game.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+public class Game extends JFrame {
+    public static void main(String[] args) {
+
+    }
+    public Game() {
+        setTitle("Vim Nav Trainer");
+        setResizable(false);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setBounds(100, 100, 900, 700);
+    }
+}
