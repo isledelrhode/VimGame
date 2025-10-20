af8d258a95c5414a4bde870797d9084b32a10995
Daniel Trotter <34330862+Trotter84@users.noreply.github.com>
2025-10-20 11:56:07 -0600

    window working on Run

--- src/Game.java
+++ src/Game.java
@@ -1,13 +1,16 @@
-import javax.swing.*;
+import javax.swing.JFrame;
+
 
 public class Game extends JFrame {
-    public static void main(String[] args) {
-
-    }
     public Game() {
         setTitle("Vim Nav Trainer");
         setResizable(false);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setBounds(100, 100, 900, 700);
+    }
+
+    public static void main(String[] args) {
+        Game frame = new Game();
+        frame.setVisible(true);
     }
 }
