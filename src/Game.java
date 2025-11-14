9875bb5860bed23cf0266c570d79cea83fc9f7ce
Daniel Trotter <34330862+Trotter84@users.noreply.github.com>
2025-11-13 17:25:59 -0700

    removed main() method from Game.java added run() method

--- src/Game.java
+++ src/Game.java
@@ -2,10 +2,23 @@ import javax.swing.JFrame;
 
 
 public class Game extends JFrame {
+<<<<<<< Updated upstream
+=======
+    public Timer timer;
+
+    public static void run() {
+        Game frame = new Game();
+        frame.setVisible(true);
+    }
+
+
+    // Constructor
+>>>>>>> Stashed changes
     public Game() {
         setTitle("Vim Nav Trainer");
         setResizable(false);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+<<<<<<< Updated upstream
         setBounds(100, 100, 900, 700);
     }
 
@@ -13,4 +26,29 @@ public class Game extends JFrame {
         Game frame = new Game();
         frame.setVisible(true);
     }
+=======
+        setBounds(0, 0, 750, 600);
+
+        // Timer
+        timer = new Timer(17, null);
+        timer.setCoalesce(false);
+        timer.start();
+
+        // Main Content Pane
+        JPanel contentPane = new JPanel();
+        contentPane.setBackground(new Color(50, 50, 52));
+        contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+        contentPane.setLayout(null);
+
+        setContentPane(contentPane);
+
+        DVDLogo dvdPanel = new DVDLogo(contentPane);
+        timer.addActionListener(dvdPanel::actionPerformed);
+        contentPane.add(dvdPanel);
+    }
+    // Helper functions
+
+
+
+>>>>>>> Stashed changes
 }
